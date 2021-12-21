43e17d085063f46d9fc3ad0e2c459c8c0c3b14b3
Mhassanbughio <muhammadhassan.sw18@muetkhp.edu.pk>
2021-12-21 14:02:30 +0500

    Container widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,31 @@ void main() {
 }
 
 class Myapp extends StatelessWidget {
+  //statless widgets
   get icons => null;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        //scaffold widget
         body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+          //Column widget
           children: <Widget>[
-            Text("home"),
-            FlatButton(
-              child: Text(
-                "sign up",
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Colors.red,
-              onPressed: () {},
-            ),
-            Text("2")
+            Container(
+                //Container wigtes
+                width: double.infinity,
+                height: 200,
+                margin: EdgeInsets.all(10),
+                color: Colors.red,
+                child: Center(
+                  child: Text(
+                    //Text widget
+                    "Its Container",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ))
           ],
         )),
         appBar: AppBar(
